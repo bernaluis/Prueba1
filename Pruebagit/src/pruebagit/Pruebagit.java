0a60f76eca98464b98f83f874ffbd0e2e6aa2d58
bernaluis <bernal.lrm@hotmail.com>
2018-01-23 00:52:13 -0600

    Segundo commit

--- Pruebagit/src/pruebagit/Pruebagit.java
+++ Pruebagit/src/pruebagit/Pruebagit.java
@@ -16,7 +16,7 @@ public class Pruebagit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola");
+        System.out.println("Hola que tal");
     }
     
     
